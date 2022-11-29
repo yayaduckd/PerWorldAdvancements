41067d194ceb243a9300fa98b04ac5141f05a595
YayaDuck <yayaduck@yayaduck.com>
2022-11-29 16:12:43 +0100

    Configurable Worlds

--- src/main/java/com/yayaduck/paper/perworldadvancements/AdvancementListener.java
+++ src/main/java/com/yayaduck/paper/perworldadvancements/AdvancementListener.java
@@ -5,6 +5,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
+
 public class AdvancementListener implements Listener {
 
     private final PerWorldAdvancements plugin;
@@ -14,7 +16,6 @@ public class AdvancementListener implements Listener {
 
     /**
      * Cancels all advancements and recipes when the player is not in survival mode.
-     *
      * This method is called when a player unlocks an advancement or recipe.
      *
      * @pre {@code event != null}
@@ -22,15 +23,36 @@ public class AdvancementListener implements Listener {
      */
     @EventHandler(priority = org.bukkit.event.EventPriority.HIGH)
     void onAdvancement(PlayerAdvancementCriterionGrantEvent event) {
-        if (event.getPlayer().getWorld().equals(Bukkit.getWorld("Creative"))) {
-            event.setCancelled(true);
-            if (plugin.getConfig().getBoolean("enableDebug")) {
-                Bukkit.getLogger().info(
-                        "PerWorldAdvancements: Advancement "
-                                + event.getAdvancement().getKey().getKey() +
-                                " was cancelled for " + event.getPlayer().getName() +
-                                " in world " + event.getPlayer().getWorld().getName() + "!");
+        ArrayList<String> worlds = (ArrayList<String>) plugin.getConfig().getStringList("worlds");
+        String playerWorld = event.getPlayer().getWorld().getName();
+
+        for (String world : worlds) {
+            if (playerWorld.equals(world)) {
+                cancelAdvancement(event);
             }
+        }
+    }
+
+    /**
+     * Cancels the advancement provided.
+     * @pre {@code event != null}
+     * @post {@code event.isCancelled() == true}
+     * @throws NullPointerException if provided event is null.
+     * @param event The advancement to cancel.
+     */
+    private void cancelAdvancement(PlayerAdvancementCriterionGrantEvent event) throws NullPointerException {
+        if (event == null) {
+            throw new NullPointerException("event cannot be null");
+        }
+
+        event.setCancelled(true);
+
+        if (plugin.getConfig().getBoolean("enableDebug")) {
+            Bukkit.getLogger().info(
+                    "PerWorldAdvancements: Advancement "
+                            + event.getAdvancement().getKey().getKey() +
+                            " was cancelled for " + event.getPlayer().getName() +
+                            " in world " + event.getPlayer().getWorld().getName() + "!");
         }
     }
 }
--- src/main/java/com/yayaduck/paper/perworldadvancements/PerWorldAdvancements.java
+++ src/main/java/com/yayaduck/paper/perworldadvancements/PerWorldAdvancements.java
@@ -1,18 +1,35 @@
 package com.yayaduck.paper.perworldadvancements;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
 
 public final class PerWorldAdvancements extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("\"PerWorldAdvancements: Enabled! 🦆");
+        setDefaults();
         getServer().getPluginManager().registerEvents(new AdvancementListener(this), this);
-
-        this.getConfig().addDefault("enableDebug", false);
-        this.saveDefaultConfig();
+        getLogger().info("PerWorldAdvancements: Enabled! 🦆");
+        getLogger().info("PerWorldAdvancements: Worlds configured: "
+                + getConfig().getStringList("worlds"));
     }
 
     @Override
     public void onDisable() {
         getLogger().info("PerWorldAdvancements: Byebye! 🦃");
+    }
+
+    private void setDefaults() {
+        FileConfiguration config = this.getConfig();
+
+        config.addDefault("enableDebug", false);
+
+        ArrayList<String> worlds = new ArrayList<>();
+        worlds.add("exampleworld1");
+        worlds.add("exampleworld2");
+        worlds.add("exampleworld2_nether");
+        config.addDefault("worlds", worlds);
+
+        saveDefaultConfig();
     }
 }
